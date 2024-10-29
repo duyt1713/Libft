066d5316207b8500b65e8ba3845b8f7caeb810b7
Duy Tung Ha <duha@c2r5p7.hive.fi>
2024-10-29 21:09:09 +0200

    backup 29.10

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: duha <duha@student.hive.fi>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 19:45:45 by duha              #+#    #+#             */
+/*   Updated: 2024/10/29 20:57:30 by duha             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+
+#endif
---
+++ strncmp.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strncmp.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: duha <duha@student.hive.fi>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 20:44:00 by duha              #+#    #+#             */
+/*   Updated: 2024/10/29 21:00:02 by duha             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (s1[i] && s2[i] && i < n)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	if (i < n)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%i\n", ft_strncmp("Duy", "duy", 6));
+	printf("%i\n", ft_strncmp("Duy", "DUy", 2));
+	printf("%i\n", ft_strncmp("Duy", "Duyy", 6));
+	printf("%i\n", ft_strncmp("Duyy", "Duy", 3));
+	printf("%i\n", ft_strncmp("Duy", "Duy", 5));
+	return (0);
+}
