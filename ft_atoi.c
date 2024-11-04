1e77e06b541267bb51e01d0a0918a5042007903c
duyt1713 <duha@student.hive.fi>
2024-11-04 01:17:38 +0000

    work on atoi 4.11

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,16 +6,36 @@
 /*   By: duha <duha@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 10:26:18 by duha              #+#    #+#             */
-/*   Updated: 2024/11/03 17:42:47 by duha             ###   ########.fr       */
+/*   Updated: 2024/11/04 01:15:28 by duha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * ft_isspace - Checks if a character is a whitespace character.
+ * @c: The character to be checked.
+ *
+ * Return: non-zero if the character is a whitespace character, 0 otherwise.
+ */
+
 static int	ft_isspace(int c)
 {
 	return (c == 32 || (9 <= c && c <= 13));
 }
+
+/**
+ * ft_atoi - Converts a string to an integer.
+ * @nptr: The string to be converted.
+ *
+ * This function converts the initial portion of the string pointed to by nptr
+ * to int representation. It discards any whitespace characters until the first
+ * non-whitespace character is found. Then, it takes an optional initial plus
+ * or minus sign followed by as many numerical digits as possible, and converts
+ * them to an integer.
+ *
+ * Return: The converted integer.
+ */
 
 int	ft_atoi(const char *nptr)
 {
@@ -39,14 +59,25 @@ int	ft_atoi(const char *nptr)
 	}
 	return (num*sign);
 }
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 int main(void)
 {
-	printf("Input 123: %i, %i\n", ft_atoi("123"), atoi("123"));
-	printf("Input -123: %i, %i\n", ft_atoi("-123"), atoi("-123"));
-	printf("Input +123: %i, %i\n", ft_atoi("+123"), atoi("+123"));
-	printf("Input -+1234: %i, %i\n", ft_atoi("-+1234"), atoi("-+1234"));
-	printf("Input +-1234: %i, %i\n", ft_atoi("+-1234"), atoi("+-1234"));
-	printf("Input ---1234: %i, %i\n", ft_atoi("---1234"), atoi("---1234"));
-	printf("Input    1234: %i, %i\n", ft_atoi("   1234"), atoi("   1234"));
-	printf("Input 1234aa5    : %i, %i\n", ft_atoi("1234aa5    "), atoi("1234aa5    "));
+    char *s = "21474836481111111111111111111111111111111111";
+    char *str;
+
+    int i = 2;
+    while (i < 40)
+    {
+        str = malloc(i + 1);
+        memcpy(str, s, i);
+        str[i] = 0;
+        printf("Input:   %s\n", str);
+        printf("atoi:    %i\n", atoi(str));
+        printf("ft_atoi: %i\n\n", ft_atoi(str));
+        free(str);
+        i++;
+    }
 }
